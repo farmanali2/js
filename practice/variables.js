4bad41b724142d92180cb5543884178e58209fd1
Farman Ali <fa.xzn080@gmail.com>
2024-12-09 09:39:07 +0000

    learned about typeof

--- practice/variables.js
+++ practice/variables.js
@@ -1,5 +1,13 @@
+"use strict"; // treat all code as newer version of JS
+
 const studentID = 8019011921
 let studentName = "Farman Ali"
 var studentCity = "New Delhi"
 
 console.table([studentID,studentName,studentCity])
+
+console.log(typeof undefined);
+
+
+// null type is object
+// undefined type is undefined
